31d870adc9b381157e8a88fb8461e49ac2d9866f
OlandoB <80422158+OlandoB@users.noreply.github.com>
2024-01-13 23:25:36 +0100

    nb occ

--- algo/Kmp.java
+++ algo/Kmp.java
@@ -4,42 +4,13 @@ import java.util.Scanner;
 
 // class permettant l'utilisation du Kmp avec les mêmess options que dans Egrep
 public class Kmp {
-    // main de test de Kmp
-    public static void main(String[] args) {
-        int [] test = lpsTable("mami");
-        for(int value : test)
-            System.out.print(value + " ");
-        System.out.println("\n le premier test affiche : " + Kmp.lineKMP("mamame mamu mia mama mi mamamia", "mami", 0, null));
-        System.out.println(" le second test affiche : " + Kmp.lineKMP("mamame mamu mia mama mi mamd", "mami", 0, null));
-    }
-
-    // Méthode pour pouvoir lire un fichier à partir d'un scanner, d'une regEx et d'une potentielle option
-    public static void myKMP(Scanner reader, String regEx,String option){
-        int nbline = 1, reslen, cptValid = 0;
-        String line;
-        String res = "";
-        // parcours de toutes les lignes et ajout a res si valide
-        while(reader.hasNextLine()) {
-            reslen = res.length();
-            line = reader.nextLine();
-            nbline++;
-            res += lineKMP(line, regEx, nbline, option);
-
-            if( reslen !=  res.length())
-                cptValid ++;
-        }
-
-        if(option != null)
-            if(option.equals("nl"))
-                System.out.println(cptValid+" lignes présentant le motif");
-        else
-            System.out.println(res);
-    }
+    
+   
 
     // création de la lps table de la regEx donnée
     public static int[] lpsTable(String regEx) {
         int[] lps = new int[regEx.length() + 1];
-        lps[0] = -1;
+        lps[0] = 0;
         int lpsI = 0; // indice courant représentant le plus long prefixe
         int tableJ = 1; // indice parcourant tous le tableau
         int regExLen = regEx.length();
@@ -58,8 +29,25 @@ public class Kmp {
         return lps;
     }
 
-    // Methode permettant l'analyse de la ligne grace a la methode KMP
-    public static String lineKMP(String line, String regEx, int cptLines, String option){
+    
+
+
+    public static void myKMPOcc(Scanner reader, String regEx){
+        int nbline = 1, cptRec = 0;
+        String line= "";
+        int res = 0;
+        // parcours de toutes les lignes et ajout a res si valide
+        while(reader.hasNextLine()) {
+            line = reader.nextLine();
+            nbline++;
+            res += lineKmpOcc(line, regEx);
+            
+        }
+        
+        System.out.println(res + " est le nombre d'oocc du mot " + regEx + " le file a " + nbline + " lignes et la ligne final est " + line);
+    }
+
+    public static int lineKmpOcc(String line, String regEx) {
         int[] lps = lpsTable(regEx); // tableau lps de la regEx
         int lineI = 0; // indice courant de la ligne
         int lpsJ = 1; // indice courant de lps
@@ -68,23 +56,20 @@ public class Kmp {
         int end = regEx.length(); // longueur de la regEx
         int lineLength = line.length(); // longueur de la ligne
         String[] words = line.split(" "); // tableau de mots pour l'option w
-
+        int occ = 0;
         while(lineI < lineLength){
             if(line.charAt(lineI) == ' ')
                 cptWord++;
             //System.out.println("Valeur de lpsJ [ " + lpsJ + " ] valeur de regExK [ " + regExK + " ] valeur de lineI [ " + lineI + " ] pour l'analyse de " + line.charAt(lineI));
             if(line.charAt(lineI) == regEx.charAt(regExK)){
                 if(regExK == end - 1) {
-                    if(option != null){
-                        if(option == "w")
-                            System.out.println("Word : " + words[cptWord] + " is check, lines " + cptLines + " word number : " + cptWord);
-                    }else
-                        System.out.println(line);
-                    return line;
+                    occ++;
+                    regExK = lps[regExK - 1];
+                    lpsJ = regExK + 1;
+                } else {
+                    lpsJ++;
+                    regExK++;
                 }
-                lpsJ++;
-                regExK++;
-
             }else {
                 lpsJ = lps[lpsJ - 1];
                 if(lpsJ == -1 || lpsJ == 0){
@@ -99,7 +84,7 @@ public class Kmp {
             }
             lineI++;
         }
-        return "";
+        return occ;
     }
 
 }
